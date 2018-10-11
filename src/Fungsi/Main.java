e6ff7d99a6417612b6dd51caa7d39aaf0929a46c
Roby <robyeka@email.unikom.ac.id>
2018-10-12 01:33:08 +0700

    First commit

---
+++ src/Fungsi/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fungsi;
+
+/**
+ *
+ * @author eka
+ */
+public class Main {
+    
+    // membuat fungsi salam
+    static void ucapSalam() {
+        System.out.println("Assalmu'alaikum");
+    }
+    
+    
+    // membuat fungsi main()
+    public static void main(String [] args) {
+        ucapSalam();
+    }
+    
+}
---
+++ src/fungsiDanParameter/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fungsiDanParameter;
+
+/**
+ *
+ * @author eka
+ */
+public class Main {
+    
+    // fungsi dengan paramter
+    static void ucapin(String ucapan) {
+        System.out.println(ucapan);
+    }
+    
+    
+    public static void main(String[] args) {
+        ucapin("hello");
+        ucapin("Selamat Datang");
+        
+    }
+    
+}
